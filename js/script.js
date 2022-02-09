b37895eb56f3bc08a1e8670ebaeee27e3029de31
Богдан Ніколайчук <nikolaychukbm@gmail.com>
2022-02-09 22:45:08 +0200

    full project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log(1)
+document.querySelector('.menu__burger').addEventListener('click', function () {
+	document.querySelector('.menu__burger').classList.toggle('active');
+	document.querySelector('.menu').classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+
+});
